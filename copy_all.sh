969a0d46602a0ed2146893d87bc5070dbb69c419
DmitryBykin <bykin2002@mail.ru>
2017-02-10 16:24:31 +0400

    добавлен hover для иконок в подвале

---
+++ copy_all.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+scp -P 1979 index.html dmitry@pd73.ru:/home/web/pd73.ru/test/
+scp -P 1979 css/* dmitry@pd73.ru:/home/web/pd73.ru/test/css/
+scp -P 1979 js/* dmitry@pd73.ru:/home/web/pd73.ru/test/js/
