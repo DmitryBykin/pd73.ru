8e3f37fe10015bf5a60fcebb2a4ea694d1d82d99
DmitryBykin <bykin2002@mail.ru>
2017-02-15 18:30:01 +0400

    Почти закончена верстка планшетной и десктопной версии

--- copy_all.sh
+++ copy_all.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-scp -P 1979 index.html dmitry@pd73.ru:/home/web/pd73.ru/test/
-scp -P 1979 css/* dmitry@pd73.ru:/home/web/pd73.ru/test/css/
-scp -P 1979 js/* dmitry@pd73.ru:/home/web/pd73.ru/test/js/
+scp -P 1979 *.html dmitry@pd73.ru:/home/web/pd73.ru/test
+scp -P 1979 css/* dmitry@pd73.ru:/home/web/pd73.ru/test/css
+scp -P 1979 js/* dmitry@pd73.ru:/home/web/pd73.ru/test/js
+scp -P 1979 images/* dmitry@pd73.ru:/home/web/pd73.ru/test/images
