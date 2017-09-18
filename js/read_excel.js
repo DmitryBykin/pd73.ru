dfafddccd50458f8c440e5ff3fd88c7bf2f508c2
DmitryBykin <bykin2002@mail.ru>
2017-09-18 23:59:50 +0400

    Добавлена функция для обработки excel

--- js/read_excel.js
+++ js/read_excel.js
@@ -1,6 +1,36 @@
-//import xlsx from 'xlsx';
+var fs = require('fs');
 var xlsx = require('xlsx');
-// Parse a file
-//const workSheetsFromFile = xlsx.parse(fs.readFileSync('js/test.xlsx'));
-const workSheetsFromFile = xlsx.readFile('js/test.xlsx');
-console.log(workSheetsFromFile.Sheets.Sheet1['A3'].w);
+const workSheetsFromFile = xlsx.readFile('shedule.xlsx');
+//console.log(workSheetsFromFile.Sheets.Sheet1['A3'].w);
+console.log(workSheetsFromFile);
+
+var resultData;
+var data = fs.readFileSync("template.html", 'utf-8');
+
+resultData = data.toString();
+//resultData+= workSheetsFromFile.Sheets.Sheet1['A3'].w;
+
+var strToChange = "";
+var numColumns = 11;
+var numRows = 20;
+var fields = ['A','B','C','D','E','F','G','H','I','J','K'];
+
+for(var row = 1; row <= numRows; row++) {
+  strToChange+= "<tr>"+"\n";
+  for(var curColumn = 0; curColumn < numColumns; curColumn++) {
+    strToChange+="<td>";
+
+    if(workSheetsFromFile.Sheets.Лист1[fields[curColumn]+row]) { // если поле не пустое
+      var par = "Лист1";
+      strToChange+= workSheetsFromFile.Sheets[par][fields[curColumn]+row].w;
+    }
+    strToChange+="</td>"+"\n";
+  }
+  strToChange+="</tr>"+"\n";
+}
+resultData = resultData.replace("{!change_here}", strToChange);
+
+fs.writeFile("result.html", resultData, function(err) {
+  if (err) throw err;
+  }
+);
